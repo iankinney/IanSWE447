f9cfec4f317b5b552b20913784437e5934439378
iankinney <Lifelessman1414@gmail.com>
2017-02-28 18:39:18 -0800

    Update Cube(1).js

--- Assignment_4_2/Cube(1).js
+++ Assignment_4_2/Cube(1).js
@@ -32,7 +32,18 @@ function Cube( vertexShaderId, fragmentShaderId ) {
     
     this.indices = { 
         values : new Uint16Array([
-            // Add your list of triangle indices here
+            (0, 3, 1),
+            (3, 1, 2),
+            (0, 1, 4),
+            (1, 4, 5),
+            (0, 4, 3),
+            (4, 3, 7),
+            (6, 5, 7), 
+            (5, 7, 4),
+            (6, 2, 5),
+            (2, 5, 1),
+            (6, 7, 2),
+            (7, 2, 3),
         ])
     };
     this.indices.count = this.indices.values.length;
