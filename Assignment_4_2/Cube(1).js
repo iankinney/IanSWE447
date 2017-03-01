72aca2bd64cd194c5f2a2d708b328429aaf1dc60
iankinney <Lifelessman1414@gmail.com>
2017-02-28 18:35:20 -0800

    Update Cube(1).js

--- Assignment_4_2/Cube(1).js
+++ Assignment_4_2/Cube(1).js
@@ -18,7 +18,15 @@ function Cube( vertexShaderId, fragmentShaderId ) {
 
     this.positions = { 
         values : new Float32Array([
-           // Add your list vertex positions here
+            (-0.5, 0.5, 0.5),
+            Vertex 1: (0.5, 0.5, 0.5)
+            Vertex 2: (0.5, -0.5, 0.5)
+            Vertex 3: (-0.5, -0.5, 0.5)
+            Vertex 4: (-0.5, 0.5, -0.5)
+            Vertex 5: (0.5, 0.5, -0.5)
+            Vertex 6: (0.5, -0.5, -0.5)
+            Vertex 7: (-0.5, -0.5, -0.5)
+
             ]),
         numComponents : 3
     };
