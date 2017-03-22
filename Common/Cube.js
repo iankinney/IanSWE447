e737268df505c521c6f5f1d707c1c4a71c89ab7b
iankinney <Lifelessman1414@gmail.com>
2017-03-21 19:58:54 -0700

    Update Cube.js

--- Common/Cube.js
+++ Common/Cube.js
@@ -31,18 +31,18 @@ function Cube( vertexShaderId, fragmentShaderId ) {
     
     this.indices = { 
         values : new Uint16Array([
-            0, 3, 1,
+            0, 1, 3,
             3, 1, 2,
-            0, 1, 4,
+            0, 4, 1,
             1, 4, 5,
-            0, 4, 3,
+            0, 3, 4,
             4, 3, 7,
             6, 5, 7, 
-            5, 7, 4,
+            7, 5, 4,
             6, 2, 5,
-            2, 5, 1,
+            5, 2, 1,
             6, 7, 2,
-            7, 2, 3
+            2, 7, 3
         ])
     };
     this.indices.count = this.indices.values.length;
